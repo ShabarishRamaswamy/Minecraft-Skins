2439f91581b16d116e70044bd08b08c1fda5e864
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2020-08-03 17:57:52 +0530

    Added Search Buttons

--- src/routers/playerDB.js
+++ src/routers/playerDB.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const axios = require('axios')
 const router = express.Router()
+// const username = require('../../public/app')
+
 // Get Username from PlayerDB
 // axios.get(`https://playerdb.co/api/player/minecraft/shabarish`)
 //     .then((res) => {
@@ -9,6 +11,11 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     res.render('index', { title: 'Hello Everyone' });
+})
+
+router.get('/:username', (req, res) => {
+    console.log(req.params.username)
+    res.render('index', { title: `Hello ${req.params.username}` });
 })
 
 router.get('/hello', (req, res) => {
